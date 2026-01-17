8b0f518826ddc298f2a154ec8da2d7a46ee5e178
Fazil Muhammad <fazilmuhammad20020719@gmail.com>
2026-01-17 15:07:57 +0530

    Update message

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
 
 // VITE_API_URL if available, otherwise fallback to the server IP.
-export const API_URL = import.meta.env.VITE_API_URL || "https://139.59.124.171:5000";
+export const API_URL = import.meta.env.VITE_API_URL || "https://api.mathersa.com";
